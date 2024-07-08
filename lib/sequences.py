1f361ec46458784abb111e0481ee564d6bf6d6d5
Nashami <wanzanashami@gmail.com>
2024-07-08 12:44:39 +0300

    Lab Complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
-
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        print([])
+    elif n == 1:
+        print([0])
+    else:
+        fibonacci_list = [0, 1]
+        while len(fibonacci_list) < n:
+            next_number = fibonacci_list[-1] + fibonacci_list[-2]
+            fibonacci_list.append(next_number)
+        print(fibonacci_list)
